f565447ed294898588a19438d56c116555d8c628
Tim Visher <tim@stitchdata.com>
2020-02-05 09:20:48 -0500

    Version 0.10.4-SNAPSHOT

--- vimgolf.el
+++ vimgolf.el
@@ -4,7 +4,7 @@
 ;; Author: Tim Visher <tim.visher@gmail.com>
 ;; Maintainer: Tim Visher <tim.visher@gmail.com>
 ;; Created: 2011-11-02
-;; Version: 0.10.3
+;; Version: 0.10.4-SNAPSHOT
 ;; Keywords: games vimgolf vim
 ;; URL: https://github.com/timvisher/vimgolf.el
 
